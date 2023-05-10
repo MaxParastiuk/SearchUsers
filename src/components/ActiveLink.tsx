a1714e304ed6107400db91d47905b3b49ad578e8
MaxParastiuk <Parastukm@gmail.com>
2023-05-10 13:18:28 +0300

    add nav active link

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface LinkProps {
 	children: string;
@@ -6,7 +8,19 @@ interface LinkProps {
 }
 
 const ActiveLink = ({ children, to }: LinkProps) => {
-	return <Link href={to}></Link>;
+	const router = usePathname();
+	const isMatch = router === to;
+	return (
+		<Link
+			href={to}
+			className={
+				isMatch
+					? "font-lemon bg-gray-600 text-lg text-red-500 hover:bg-blue-800 rounded-md p-3 duration-500"
+					: " font-lemon bg-gray-600 text-lg text-white hover:bg-blue-800 rounded-md p-3 duration-500"
+			}>
+			{children}
+		</Link>
+	);
 };
 
 export default ActiveLink;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
 				<nav className='z-10 bg-gray-400'>
 					<div className='mx-24 max-[640px]:mx-6 min-[320px]:py-6 max-w-full px-4 sm:px-6 lg:px-8'>
 						<div className='flex min-[320px]:gap-y-5 lg:flex-row min-[320px]:flex-col h-full items-center justify-between'>
-							<div className='flex items-center  min-[320px]:flex-col'>
+							<div className='flex items-center  min-[320px]:flex-col basis-60 items-baseline'>
 								<div className='flex-shrink-0'>
 									<Image
 										width={800}
@@ -20,13 +20,15 @@ function Header() {
 										alt='user-logo'
 									/>
 								</div>
-								<div className='md:block'>
-									<div className='ml-10 flex items-baseline space-x-4'>
-										<ActiveLink to='/'>Users</ActiveLink>
-									</div>
-								</div>
 							</div>
 							<SearchForm />
+
+							<div className=''>
+								<div className=' flex items-baseline space-x-4'>
+									<ActiveLink to='/'>Home</ActiveLink>
+									<ActiveLink to='/users/1'>Users List</ActiveLink>
+								</div>
+							</div>
 						</div>
 					</div>
 				</nav>
